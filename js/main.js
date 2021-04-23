ef56fde4577afaf0632d7cc5559ffe0766d78db4
ix83 <neon.trip@yandex.com>
2021-04-24 04:13:16 +0700

    media up to 1194

--- js/main.js
+++ js/main.js
@@ -13,14 +13,37 @@ let swiper = new Swiper('.swiper-container', {
 const scrollToTopButton = document.getElementById('scrollToTop')
 const featuresSection = document.getElementById('features')
 const scrollToElementButton = document.getElementById('scrollDown')
+const mobileOpenButton = document.getElementById('mobileOpen')
+const mobileCloseButton = document.getElementById('mobileClose')
+const mobileOverlay = document.getElementById('mobileOverlay')
+const mobileMenu = document.getElementById('mobileMenu')
+const mobileNav = document.getElementById('mobileNav')
 
 function scrollToTop() {
   window.scrollTo({top: 0, behavior: 'smooth'});
 }
-
-
-function scrollToElement(){
+function scrollToElement() {
   featuresSection.scrollIntoView({block: "center", behavior: "smooth"})
+}
+function openMobile() {
+  mobileOpenButton.style.display = 'none';
+  mobileMenu.style.display = 'flex'
+  mobileNav.style.display = 'flex'
+  mobileOverlay.classList.add('mobile__overlay_visible')
+  mobileCloseButton.style.display = 'block'
+}
+function closeMobile() {
+  mobileOpenButton.style.display = 'block';
+  mobileMenu.style.display = 'none'
+  mobileNav.style.display = 'none'
+  mobileOverlay.classList.remove('mobile__overlay_visible')
+  mobileOverlay.classList.add('mobile__overlay')
+  mobileCloseButton.style.display = 'none'
 }
 scrollToTopButton.addEventListener('click', scrollToTop)
 scrollToElementButton.addEventListener('click', scrollToElement)
+mobileOpenButton.addEventListener('click', openMobile)
+mobileCloseButton.addEventListener('click', closeMobile)
+window.addEventListener('keydown', e => {
+  if (e.key == 'Escape') closeMobile(e)
+})
